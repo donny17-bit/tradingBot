814ec75825d8d759077299ce95b0d07b5f564eda
donny <doniwahyu14@gmail.com>
2025-02-22 22:39:55 +0700

    v1.0.0 - first deploy

---
+++ main.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = process.env.PORT;
+
+// Middleware to parse JSON
+app.use(bodyParser.json());
+
+const positionsRouter = require("./routes/positions");
+const webhookRouter = require("./routes/webhook");
+const contractRouter = require("./routes/contract");
+const orderRouter = require("./routes/order");
+const tickerRouter = require("./routes/ticker");
+
+app.use(positionsRouter);
+app.use(webhookRouter);
+app.use(contractRouter);
+app.use(orderRouter);
+app.use(tickerRouter);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ routes/order.js
@@ -0,0 +1,153 @@
+const express = require("express");
+const crypto = require("crypto");
+const axios = require("axios");
+
+const router = express.Router();
+
+router.post("/place-order", async (req, res) => {
+  function option(method, path, queryString, body) {
+    const hmac = crypto.createHmac("sha256", process.env.API_SECRET);
+    const timestamp = Math.round(new Date());
+    let pre_hash = "";
+
+    if (method === "GET") {
+      pre_hash = timestamp + method + path + queryString;
+    } else {
+      const stringBody = JSON.stringify(body);
+      pre_hash = timestamp + method + path + stringBody;
+    }
+
+    const signature = hmac.update(pre_hash).digest("base64");
+
+    const options = {
+      headers: {
+        "Content-Type": "application/json",
+        "ACCESS-KEY": process.env.API_KEY,
+        "ACCESS-SIGN": signature,
+        "ACCESS-TIMESTAMP": timestamp,
+        "ACCESS-PASSPHRASE": process.env.API_PASSPHRASE,
+        locale: "en-US",
+      },
+    };
+
+    return options;
+  }
+
+  // get current price
+  async function getCurrentPrice() {
+    const options = option(
+      "GET",
+      "/api/v2/mix/market/ticker",
+      "?productType=USDT-FUTURES&symbol=BTCUSDT",
+      undefined
+    );
+
+    try {
+      const res = await axios.get(
+        `${process.env.API_DOMAIN}/api/v2/mix/market/ticker?productType=USDT-FUTURES&symbol=BTCUSDT`,
+        options
+      );
+
+      return res.data.data[0].lastPr;
+    } catch (error) {
+      console.error("Error fetching price:", error);
+      res.status(500).json({ message: "Error fetching price" });
+      return 0;
+    }
+  }
+
+  //  get single position
+  async function getSinglePosition() {
+    const options = option(
+      "GET",
+      "/api/v2/mix/position/single-position",
+      "?productType=USDT-FUTURES&symbol=BTCUSDT&marginCoin=USDT",
+      undefined
+    );
+
+    try {
+      const res = await axios.get(
+        `${process.env.API_DOMAIN}/api/v2/mix/position/single-position?productType=USDT-FUTURES&symbol=BTCUSDT&marginCoin=USDT`,
+        options
+      );
+
+      return res.data.data;
+    } catch (error) {
+      console.error("Error fetching positions:", error);
+      res.status(500).json({ message: "Error fetching positions" });
+      return 0;
+    }
+  }
+
+  try {
+    const price = await getCurrentPrice();
+    const leverage = process.env.LEVERAGE;
+    const size = parseFloat(leverage) / parseFloat(price);
+
+    let body = {
+      symbol: "BTCUSDT",
+      productType: "USDT-FUTURES",
+      marginMode: "isolated",
+      marginCoin: "USDT",
+      size: size,
+      side: req.body.action,
+      tradeSide: "open",
+      orderType: "market",
+    };
+
+    // check if there is an open position
+    const position = await getSinglePosition();
+    if (position.length > 0) {
+      // close the position
+      body.side = position[0].holdSide === "long" ? "buy" : "sell";
+      body.tradeSide = "close";
+      body.size = position[0].total;
+      const options = option(
+        "POST",
+        "/api/v2/mix/order/place-order",
+        undefined,
+        body
+      );
+
+      const orderClosed = await axios.post(
+        `${process.env.API_DOMAIN}/api/v2/mix/order/place-order`,
+        body,
+        options
+      );
+
+      console.log(
+        `${position[0].holdSide} order closed successfully : `,
+        orderClosed.data
+      );
+    }
+
+    //  place the new order
+    body.side = req.body.action;
+    body.tradeSide = "open";
+    body.size = size;
+    const options = option(
+      "POST",
+      "/api/v2/mix/order/place-order",
+      undefined,
+      body
+    );
+    await axios
+      .post(
+        `${process.env.API_DOMAIN}/api/v2/mix/order/place-order`,
+        body,
+        options
+      )
+      .then((response) => {
+        console.log(
+          `${req.body.action} order placed successfully : `,
+          response.data
+        );
+        res.status(200).json(response.data);
+      });
+  } catch (error) {
+    console.error("Error placing order:", error);
+    res.status(500).json({ message: "Error placing order" });
+  }
+});
+
+module.exports = router;
