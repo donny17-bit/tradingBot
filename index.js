24b25d1aaae5a9f494917566ecb4d615bbe6d70a
donny <doniwahyu14@gmail.com>
2025-02-22 22:51:38 +0700

    v1.0.2 - recommit

--- index.js
+++ index.js
@@ -8,17 +8,8 @@ const PORT = process.env.PORT;
 // Middleware to parse JSON
 app.use(bodyParser.json());
 
-const positionsRouter = require("./routes/positions");
-const webhookRouter = require("./routes/webhook");
-const contractRouter = require("./routes/contract");
 const orderRouter = require("./routes/order");
-const tickerRouter = require("./routes/ticker");
-
-app.use(positionsRouter);
-app.use(webhookRouter);
-app.use(contractRouter);
 app.use(orderRouter);
-app.use(tickerRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
